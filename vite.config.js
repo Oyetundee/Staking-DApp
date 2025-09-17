f708cc67cc0a1c26ef44e25b97e447b76267f456
Oyetundee <ayeti78@gmail.com>
2025-09-17 15:45:42 +0100

    fix vite config for apollo client

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [],
+    }
+  },
   optimizeDeps: {
-    include: ['@apollo/client', 'rxjs']
+    include: ['@apollo/client', 'rxjs', 'rxjs/operators']
+  },
+  define: {
+    global: 'globalThis',
   }
 })
